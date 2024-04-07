d6a2bd349c366f60c9da534f1862830ac69d376d
Ravend1028 <ravendavid.rd30@gmail.com>
2024-04-08 03:16:27 +0800

    finished home page

--- home.php
+++ home.php
@@ -1,8 +1,31 @@
 <?php include 'incs/header.php'; ?>
 
   <!-- Home Section -->
-  <section class="p-3">
-      <video src="videos/#" class="object-fit-cover w-100" autoplay loop></video>
+  <section class="home_section d-flex justify-content-center position-relative" style="height: 600px;">
+      <video src="videos/CDM_1NTRO.mp4" class="object-fit-cover border rounded w-100" autoplay loop></video>
+      
+      <div class="d-flex flex-column justify-content-center align-items-center container w-100 h-100 position-absolute z-2">
+        <div class="d-flex flex-column justify-content-center align-items-center border rounded p-3" style="background-color: rgba(0, 0, 0, 0.5); backdrop-filter: blur(5px);">
+          <h1 class="text-white mb-3 text-center w-75">
+            <span class="text-warning">
+              PATHFIT:
+            </span> 
+            Event Registration Portal for 
+            Colegio De Muntinlupa
+          </h1>
+
+          <h5 class="text-white mb-3">
+            Welcome to PathFit Portal: Where Every Step Counts! Ready to join our upcoming events and dive into the world of sports and fitness? PathFit Portal offers seamless event registration for activities like basketball, volleyball, and more, tailored to enthusiasts of all levels. Whether you're a seasoned player or just starting out, our user-friendly registration process ensures you can easily secure your spot in the action-packed events we have in store. Join us today and let's shape a brighter, fitter future together!
+          </h5>
+
+          <div class="d-flex justify-content-center">
+            <button class="btn btn-dark btn-lg my-2" type="button">
+              <i class="bi bi-chevron-right"></i>
+              Register Now
+            </button>
+          </div>
+        </div>
+      </div>   
   </section>
 
 <?php include 'incs/footer.php'; ?>
